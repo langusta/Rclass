ad7de0d723e125c1e6795fe9430b59e0c8d8d6e9
langusta <kontokarlosa@yahoo.pl>
2014-11-24 13:21:31 +0100

    - wsdm 1 lekcja

---
+++ wsdm.R
@@ -0,0 +1,16 @@
+####  WSDM - web scale data mining
+
+## Free data sources:
+- http://dumps.wikimedia.org - content of wikipedia
+  - projects using this data - http://en.wikipedia.org/wiki/Wikipedia:ACST
+- https://dev.twitter.com/docs - tweets and metadata available through REST API (responses formatted in JSON)
+- http://www.githubarchive.org/ - commits, pulls, requests, forks, team membership available in JSON format, also available through Google BigQuery
+- http://data.stackexchange.com/help - content of Stack Overflow available via BitTorrent on CC-BY-SA license
+- http://aws.amazon.com/publicdatasets/ - collection of huge datasets- they charge for data downloads! - it is possible to get promotional credit coupons, examples:
+  - Common Crawl - corpus of web crawl data,
+  - 1000 Genomes Project - map of human genetic information,
+  - Google Book N-grams,
+  - Million Song Dataset
+  - ...
+- http://data.gov/ - over 100k of datasets from US government,
+- http://data.gov.uk/ - similar to above
