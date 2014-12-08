d8ae2e00af6b8e741166db9e833db36b2f217a3e
langusta <kontokarlosa@yahoo.pl>
2014-12-08 16:56:21 +0100

    homework 2 c.d.

--- wsdm/wsdm_homework2_naiveBayesCode_kubicki.R
+++ wsdm/wsdm_homework2_naiveBayesCode_kubicki.R
@@ -20,10 +20,12 @@
 
 ## 2. Read data:
 
+pathTest<-"/home/user/Projects/ContentBigData/Dane/wsdm/WSDM_H2_randomSampleOfDocuments/test"
+pathTrain<-"/home/user/Projects/ContentBigData/Dane/wsdm/WSDM_H2_randomSampleOfDocuments/train"
 
 readFilesFromPath <- function(path){
   library(stringi)
-  fnames_test <- list.files("/home/user/Projects/ContentBigData/Dane/wsdm/WSDM_H2_randomSampleOfDocuments/test",
+  fnames_test <- list.files(path,
                             pattern= "*.txt", full.names=TRUE)
   names<-character(length(fnames_test))
   rating<- numeric(length(fnames_test))
@@ -31,6 +33,18 @@ readFilesFromPath <- function(path){
   body <- character(length(fnames_test))
   for(k in seq_along(fnames_test)){
       names[k]<- stri_extract_first_regex(fnames_test[k], "[01-9_]+(?=\\.txt)")
-      sprawdzam ssh
+      body[k] <- readChar(fnames_test[k], file.info(fnames_test[k])$size)
+      rating[k] <- as.numeric(stri_extract_first_regex(names[k], "(?<=[_])[01-9]+"))
+      label[k] <- ifelse(rating[k]<5,0,1)
   }
+  data.frame(names=names, body=body, rating=rating, label=label, stringsAsFactors = FALSE)
 }
+
+train <- readFilesFromPath(pathTrain)
+test <- readFilesFromPath(pathTest)
+
+## Generate class probabilities:
+## Genarete dictionary with word probability for each class
+
+
+
