fd0653595e00a3504abed2234ce96c0e956776a1
langusta <kontokarlosa@yahoo.pl>
2015-01-13 09:49:00 +0100

    zmiany w test_przedzial_ufnosci

--- test_przedzial_ufnosci.R
+++ test_przedzial_ufnosci.R
@@ -1,8 +1,9 @@
 
-values <- 0:9
-prob <- c(40,100,15,5,2,1,1,1,1,1)
+values <- c(0:9,20,25,30)
+prob <- c(40,100,15,5,2,1,1,1,1,1,1,1,1)
 prob <- prob/sum(prob)
-N <- 1000000
+N <- 3000000
+xpois <- rpois(N, mean(x))
 
 x<- numeric(0)
 for (k in seq_along(values)){
@@ -10,15 +11,32 @@ for (k in seq_along(values)){
 }
 Nr <- length(x)
 
-n <- 5000
-l <- 1000
+n <-1000
+l <- 1500
 means <- numeric(l)
-for (k in 1:l){
+sds <- numeric(l)
+# ns <- seq(from = 1000, to = 50000, by = 100)
+for (k in 1:l){ #seq_along(ns)){
+   # n<-ns[k]
    means[k] <- mean(sample(x, n))
+   sds[k] <- sd(sample(x, n))
 }
 
 # http://stackoverflow.com/questions/7781798/seeing-if-data-is-normally-distributed-in-r
 #hist(means)
+k<- 1
+plot(density(means), col = "red")
+lines( xx <- seq(from = mean(means) - 3*sd(means), 
+                  to = mean(means) + 3*sd(means), length.out = 2000),
+      dnorm(xx, mean(means), sd(means)),
+      col = "blue"
+      )
+abline(v=mean(x))
 qqnorm(means)
 qqline(means)
-dpois(1, mean(x))
+
+mean(means)
+mean(x)
+
+plot(0:9, summary(as.factor(x)), col = "red")
+points(0:9, summary(as.factor(xpois[xpois<10])) ,col="blue")
